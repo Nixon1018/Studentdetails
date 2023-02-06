f500c752951f3b77819176099c72dea694e083c0
Nixon1018 <nixon.joshwa@tringapps.com>
2023-02-06 14:18:21 +0530

    firsst

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -28,35 +28,37 @@ public class Main {
         int truee=1;
 
         while(truee!=0) {
-            LOGGER.info("1.add details 2.show 3.exit");
-            int check=sc.nextInt();
-            if(check==1) {
-                LOGGER.info("Enter the name");
-                name = sc.next();
-                LOGGER.info("Enter the age");
-                age = sc.nextInt();
-                LOGGER.info("Enter the gpa");
-                gpa = sc.nextDouble();
-                test.add(new Student(name, age, gpa));
-                Collections.sort(test, new Comparator<Student>() {
-                    public int compare(Student s1, Student s2) {
-                        return Double.compare(s2.gpa, s1.gpa);
+            try {
+                LOGGER.info("1.add details 2.show 3.exit");
+                int check = sc.nextInt();
+                if (check == 1) {
+                    LOGGER.info("Enter the name");
+                    name = sc.next();
+                    LOGGER.info("Enter the age");
+                    age = sc.nextInt();
+                    LOGGER.info("Enter the gpa");
+                    gpa = sc.nextDouble();
+                    test.add(new Student(name, age, gpa));
+                    Collections.sort(test, new Comparator<Student>() {
+                        public int compare(Student s1, Student s2) {
+                            return Double.compare(s2.gpa, s1.gpa);
+                        }
+
+
+                    });
+                } else if (check == 2) {
+                    for (int i = 0; i < test.size(); i += 1) {
+                        String z = test.get(i).name + " " + test.get(i).age + " " + test.get(i).gpa;
+                        LOGGER.info(z);
                     }
-
-
-                });
-            }
-             else if(check==2)
-            {
-                for(int i=0;i<test.size();i+=1) {
-                    String z = test.get(i).name +" "+test.get(i).age+" "+test.get(i).gpa;
-                    LOGGER.info(z);
+                } else if (check == 3) {
+                    truee = 0;
                 }
-            }
-
-            else if(check==3)
+            } catch (Exception e)
             {
-                truee=0;
+                String ec = ""+e;
+                LOGGER.info(ec);
+                sc.nextLine();
             }
         }
 
