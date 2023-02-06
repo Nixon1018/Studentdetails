c0d90d0b578f39919f013104506189e9624523b9
Nixon1018 <nixon.joshwa@tringapps.com>
2023-02-06 13:43:24 +0530

    firsst

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,63 @@
-package org.example;public class Main {
+package org.example;
+
+import java.util.*;
+import java.util.Scanner;
+import java.util.logging.Logger;
+
+class Student{
+    String name;
+    int age;
+    double gpa;
+    Student(String name,int age,double gpa)
+    {
+        this.name=name;
+        this.age=age;
+        this.gpa=gpa;
+    }
+}
+
+public class Main {
+    private static final Logger LOGGER=Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<Student> test = new ArrayList<Student>();
+
+        Scanner sc= new Scanner(System.in);
+        String name;
+        int age;
+        double gpa;
+        int truee=1;
+
+        while(truee!=0) {
+            LOGGER.info("1.add details 2.show 3.exit");
+            int check=sc.nextInt();
+            if(check==1) {
+                LOGGER.info("Enter the name");
+                name = sc.next();
+                LOGGER.info("Enter the age");
+                age = sc.nextInt();
+                LOGGER.info("Enter the gpa");
+                gpa = sc.nextDouble();
+                test.add(new Student(name, age, gpa));
+                Collections.sort(test, new Comparator<Student>() {
+                    public int compare(Student s1, Student s2) {
+                        return Double.compare(s2.gpa, s1.gpa);
+                    }
+
+
+                });
+            }
+             else if(check==2)
+            {
+                for(int i=0;i<test.size();i+=1) {
+                    LOGGER.info(test.get(i).name +" "+test.get(i).age+" "+test.get(i).gpa);
+                }
+            }
+
+            else if(check==3)
+            {
+                truee=0;
+            }
+        }
+
     }
 }
